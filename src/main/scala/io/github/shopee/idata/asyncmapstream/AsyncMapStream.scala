5b21b8914dd46c9c275b159e09468fe4a7779206
loveKino <842914439@qq.com>
2019-01-08 17:34:05 +0800

    make model simpler

--- src/main/scala/io/github/shopee/idata/asyncmapstream/AsyncMapStream.scala
+++ src/main/scala/io/github/shopee/idata/asyncmapstream/AsyncMapStream.scala
@@ -1,5 +1,6 @@
 package io.github.shopee.idata.asyncmapstream
 
+import java.util.concurrent.Executors
 import scala.collection.mutable.SynchronizedQueue
 import scala.collection.convert.decorateAsScala._
 import scala.concurrent.{ Future }
@@ -11,168 +12,179 @@ import scala.concurrent.ExecutionContext
   *
   * problem:
   *   if stream s1 is fast, but computation is slow, may cause blocking problem.
-  *
-  *
-  * producer: fast stream
-  *
-  * calculator, map function: (T) -> T
-  *
-  * collector: collect results, and output as a stream
   */
-case class AsyncMapStreamRecord(record: Any, signal: Int)
-
+// TODO error and close
 object AsyncMapStream {
+  val SIGNAL_END      = 1
+  val SIGNAL_HOLD     = 2
+  val SIGNAL_RESOLVED = 3
+
   type MapFunction[T, U] = (T) => U
+  type ItemHandler[U]    = (U) => Any
+  type EndHandler        = () => Any
+  type resolveCallback   = (Exception) => _
 
-  val SIGNAL_ITEM = 0
-  val SIGNAL_END  = 1
+  case class ConsumerSignal(var signal: Int = SIGNAL_HOLD,
+                            var extra: Any = null,
+                            resolveCallback: resolveCallback) {
+    def resolve(err: Exception, data: Any) = {
+      extra = data
+      signal = SIGNAL_RESOLVED
 
-  case class SendingQueue(bucketIndex: Int, sendItem: (AsyncMapStreamRecord, Int) => Future[_])(
-      implicit ec: ExecutionContext
-  ) {
-    private val queue = new SynchronizedQueue[AsyncMapStreamRecord]()
-
-    def enqueue(record: AsyncMapStreamRecord) = {
-      queue.enqueue(record)
-      sendRecord()
-    }
-
-    private var prevSending: Future[_] = null
-
-    private def sendRecord(): Unit = synchronized {
-      if (queue.length > 0 && prevSending == null) {
-        val record = queue.dequeue()
-        // send current record to a map calculator
-        prevSending = sendItem(record, bucketIndex)
-
-        prevSending map { _ =>
-          prevSending = null
-          sendRecord()
-        }
-      }
+      resolveCallback(err)
     }
   }
 
-  /**
-    * responsible for map computation of stream records
-    */
-  case class MapCalculator[T, U](
-      mapFunction: MapFunction[T, U],
-      bucketIndex: Int,
-      gather: (AsyncMapStreamRecord, Int) => Future[_]
-  )(implicit ec: ExecutionContext) {
-    private val sendingQueue = SendingQueue(bucketIndex, gather)
-
-    def calculate(record: AsyncMapStreamRecord) =
-      record.signal match {
-        case SIGNAL_ITEM =>
-          val result = mapFunction(record.record.asInstanceOf[T])
-          // send result to collector
-          sendingQueue.enqueue(AsyncMapStreamRecord(result, SIGNAL_ITEM))
-        case SIGNAL_END =>
-          sendingQueue.enqueue(record)
-      }
-  }
+  type Mapper = (ConsumerSignal) => _
 
   // TODO failure tolerant
-  case class Consumer[T](buckets: Int, sendItem: (AsyncMapStreamRecord, Int) => Future[_])(
+  case class Consumer[T, U](mapper: Mapper,
+                            itemHandler: ItemHandler[U],
+                            endHandler: EndHandler,
+                            // TODO error handler
+                            buckets: Int = 8)(
       implicit ec: ExecutionContext
   ) {
-    private var currentIndex: Long = 0
-
-    private val bucketQueues = 0 to buckets - 1 map (
-        (bucketIndex) => SendingQueue(bucketIndex, sendItem)
+    private val bucketQueues = 1 to buckets map (
+        (item) => new SynchronizedQueue[ConsumerSignal]()
     )
+
+    private var inputPointer: Int = 0
 
     /**
       * stream is ordered, output one by one.
       * Consume record one by one too.
       */
-    def consume(record: T): Unit = synchronized {
-      consumeHelp(AsyncMapStreamRecord(record, SIGNAL_ITEM))
+    def consume(record: T): Unit = {
+      // run map function on current record
+      runMapOnRecord(record, inputPointer)
+
+      // to next bucket
+      inputPointer = (inputPointer + 1) % buckets
     }
 
-    private def consumeHelp(record: AsyncMapStreamRecord): Unit = synchronized {
-      val bucketIndex = (currentIndex % buckets).toInt
-      bucketQueues(bucketIndex).enqueue(record)
-      currentIndex += 1
+    def runMapOnRecord(record: T, bucketIndex: Int) = {
+      // push a holder to queue
+      val signal = ConsumerSignal(SIGNAL_HOLD, record, resolveCallback)
+      bucketQueues(bucketIndex).enqueue(signal)
+
+      mapper(signal)
+    }
+
+    def resolveCallback(err: Exception) =
+      if (err != null) {
+        // TODO
+      } else {
+        collect()
+      }
+
+    private var outputPointer = 0 // rotate outputPointer from 0 to buckets - 1
+
+    private def collect() = synchronized {
+      // output record
+      var pointedQueue = bucketQueues(outputPointer)
+      var cont         = true
+
+      while (cont && pointedQueue.length > 0) {
+        val signal = pointedQueue.front
+
+        signal.signal match {
+          case SIGNAL_HOLD => {
+            // break the loop
+            cont = false
+          }
+
+          case SIGNAL_RESOLVED => {
+            pointedQueue.dequeue()
+            itemHandler(signal.extra.asInstanceOf[U])
+          }
+
+          case SIGNAL_END => {
+            pointedQueue.dequeue()
+            endHandler()
+          }
+        }
+
+        if (cont) {
+          // rotate outputPointer
+          outputPointer = (outputPointer + 1) % buckets
+          pointedQueue = bucketQueues(outputPointer)
+        }
+      }
     }
 
     // finished stream signal
-    def finish() =
-      consumeHelp(AsyncMapStreamRecord(null, SIGNAL_END))
+    def finish() = {
+      bucketQueues(inputPointer).enqueue(ConsumerSignal(SIGNAL_END, null, resolveCallback))
+      collect()
+
+      // to next bucket
+      inputPointer = (inputPointer + 1) % buckets
+    }
+  }
+
+  case class LocalSerialMapper[T, U](mapFunction: MapFunction[T, U])(
+      implicit ec: ExecutionContext
+  ) {
+    val queue = new SynchronizedQueue[ConsumerSignal]()
+
+    var isProcessing = false
+
+    private def mapQueue() = synchronized {
+      if (!isProcessing) {
+        // lock
+        isProcessing = true
+        Future {
+          // println(s"queue length: ${queue.length}, thread id is: ${Thread.currentThread().getId()}")
+          while (queue.length > 0) {
+            val signal = queue.dequeue()
+            try {
+              signal.resolve(null, mapFunction(signal.extra.asInstanceOf[T]))
+            } catch {
+              case e: Exception => signal.resolve(e, null)
+            }
+          }
+          // release
+          isProcessing = false
+        }
+      }
+    }
+
+    val mapper: Mapper = (signal: ConsumerSignal) => {
+      queue.enqueue(signal)
+      mapQueue()
+    }
   }
 
   /**
-    * collect consumer's result and output as a new stream with the same input order
+    * special case for local process computation
     */
-  case class Collecter[U](buckets: Int, itemHandler: (U) => Any, endHandler: () => Any) {
-    private val bucketQueues = 1 to buckets map (
-        (item) => new SynchronizedQueue[AsyncMapStreamRecord]()
-    )
-    private var pointer = 0 // rotate pointer from 0 to buckets - 1
-
-    def collect(recordResult: AsyncMapStreamRecord, bucketIndex: Int) = {
-      // push record result to specified bucket
-      bucketQueues(bucketIndex).enqueue(recordResult)
-      // output record
-      outputRecord()
+  case class LocalProcesser[T, U](buckets: Int = 8, executor: ExecutionContext = null) {
+    implicit val ec = if (executor == null) {
+      // default thread pool
+      ExecutionContext.fromExecutor(Executors.newFixedThreadPool(buckets))
+    } else {
+      executor
     }
 
-    private def outputRecord(): Unit = synchronized {
-      val pointedQueue = bucketQueues(pointer)
+    def process(mapFunction: MapFunction[T, U],
+                itemHandler: ItemHandler[U],
+                endHandler: EndHandler): Consumer[T, U] = {
+      val localSerialMappers = 1 to buckets map { _ =>
+        LocalSerialMapper(mapFunction)
+      }
+      var pointer = 0
 
-      if (pointedQueue.length > 0) {
-        val record = pointedQueue.dequeue()
-        record.signal match {
-          case SIGNAL_ITEM =>
-            try {
-              itemHandler(record.record.asInstanceOf[U])
-            } catch {
-              case e: Exception => {
-                //
-              }
-            }
-          case SIGNAL_END =>
-            endHandler()
-        }
-        // rotate pointer
+      val mapper: Mapper = (signal: ConsumerSignal) => {
+        localSerialMappers(pointer).mapper(signal)
+
         pointer += 1
         pointer = pointer % buckets
-
-        outputRecord()
       }
+
+      val consumer = Consumer[T, U](mapper, itemHandler, endHandler, buckets)
+
+      consumer
     }
-  }
-
-  /**
-    * special case for single process computation
-    */
-  def singleProcess[T, U](mapFunction: MapFunction[T, U],
-                       itemHandler: (U) => Any,
-                       endHandler: () => Any,
-                       buckets: Int = 8)(implicit ec: ExecutionContext): Consumer[T] = {
-
-    // collector
-    val collecter = Collecter[U](buckets, itemHandler, endHandler)
-
-    // create some calculators
-    val gather = (recordResult: AsyncMapStreamRecord, bucketIndex: Int) => {
-      Future {
-        collecter.collect(recordResult, bucketIndex)
-      }
-    }
-    val cals = 0 to buckets - 1 map (
-        (bucketIndex) => MapCalculator[T, U](mapFunction, bucketIndex, gather)
-    )
-
-    // consumer
-    val sendItem = (record: AsyncMapStreamRecord, bucketIndex: Int) => {
-      Future {
-        cals(bucketIndex).calculate(record)
-      }
-    }
-
-    Consumer[T](buckets, sendItem)
   }
 }
--- src/test/scala/io/github/shopee/idata/asyncmapstream/AsyncMapStreamTest.scala
+++ src/test/scala/io/github/shopee/idata/asyncmapstream/AsyncMapStreamTest.scala
@@ -1,39 +1,93 @@
 package io.github.shopee.idata.asyncmapstream
 
 import scala.concurrent.{ Await, Future, Promise, duration }
-import scala.concurrent.ExecutionContext.Implicits.global
 import scala.collection.mutable.ListBuffer
 import duration._
 
 class AsyncMapStreamTest extends org.scalatest.FunSuite {
   import AsyncMapStream._
 
-  test("single process") {
-    val expectList  = 1 to 1000 map ((item) => item * item + "")
-    val mapFunction = (v: Int) => (v * v) + ""
-
-    1 to 1000 foreach { index =>
-      val p          = Promise[Int]()
-      val resultList = ListBuffer[String]()
-      val consumer = singleProcess[Int, String](mapFunction, (record) => {
-        resultList.append(record)
-      }, () => {
-        p.trySuccess(0)
-      })
-      // stream
-      1 to 1000 foreach { item =>
-        consumer.consume(item)
-      }
-      // end signal
-      consumer.finish()
-      Await.result(p.future, Duration.Inf)
-      assert(resultList.length == expectList.length)
-      assert(resultList == expectList)
+  def identityTime[T](list: List[T], log: Boolean = false) = {
+    val t1         = System.currentTimeMillis()
+    val resultList = ListBuffer[T]()
+    list.map((item) => {
+      resultList.append(item)
+    })
+    val t2 = System.currentTimeMillis()
+    if (log) {
+      println(s"map identity, only append to list buffer: ${t2 - t1}")
     }
   }
 
+  def testLocalAsyncMapStream[T, U](list: List[T],
+                                    singleProcess: LocalProcesser[T, U],
+                                    mapFunction: MapFunction[T, U],
+                                    log: Boolean = false) = {
+    identityTime(list, log)
+    val p          = Promise[Int]()
+    val resultList = ListBuffer[U]()
+    val t1         = System.currentTimeMillis()
+    val consumer = singleProcess.process(
+      mapFunction,
+      (record) => {
+        resultList.append(record)
+      },
+      () => {
+        val t2 = System.currentTimeMillis()
+        if (log) {
+          println(s"concurrent solution: ${t2 - t1}")
+        }
+        p.trySuccess(0)
+      }
+    )
+
+    val t6 = System.currentTimeMillis()
+    // stream
+    list foreach { item =>
+      consumer.consume(item)
+    }
+    // end signal
+    consumer.finish()
+    val t5 = System.currentTimeMillis()
+    if (log) {
+      println(s"concurrent solution - source end time: ${t5 - t6}")
+    }
+
+    Await.result(p.future, Duration.Inf)
+
+    // normal computation
+    val t3           = System.currentTimeMillis()
+    val expectedList = ListBuffer[U]()
+    list.map(mapFunction).foreach(item => expectedList.append(item))
+    val t4 = System.currentTimeMillis()
+
+    if (log) {
+      println(s"normal solution: ${t4 - t3}")
+    }
+    // assert(resultList.length == expectedList.length)
+
+    // assert(resultList == expectedList)
+  }
+
+  test("single process") {
+    val mapFunction   = (v: Int) => (v * v) + ""
+    val singleProcess = LocalProcesser[Int, String]()
+
+    1 to 1000 foreach { index =>
+      testLocalAsyncMapStream[Int, String]((1 to 1000).toList, singleProcess, mapFunction)
+    }
+  }
+
+  test("big integer array") {
+    val mapFunction   = (v: Int) => (v.toString().split("").mkString("").toInt * v) + ""
+    val singleProcess = LocalProcesser[Int, String]()
+
+    testLocalAsyncMapStream[Int, String]((1 to 1000000).toList, singleProcess, mapFunction, true)
+  }
+
   test("perf: long string operations") {
-    val longText = 1 to 10000 map ((index) => s"${index}") mkString (",")
+    val longText      = 1 to 10000 map ((index) => s"${index}") mkString (",")
+    val singleProcess = LocalProcesser[String, String]()
 
     val mapFunction =
       (v: String) => {
@@ -44,34 +98,9 @@ class AsyncMapStreamTest extends org.scalatest.FunSuite {
         result
       }
 
-    var t1         = System.currentTimeMillis()
-    val p          = Promise[String]()
-    val resultList = ListBuffer[String]()
-    val consumer = singleProcess[String, String](mapFunction, (record) => {
-      resultList.append(record)
-    }, () => {
-      p.trySuccess("")
-    }, 32)
-    // handle 1000 strings
-    1 to 1000 foreach { _ =>
-      consumer.consume(longText + ";")
-    }
-    // end signal
-    consumer.finish()
-
-    Await.result(p.future, Duration.Inf)
-    var t2 = System.currentTimeMillis()
-    println(s"concurrent map computation: ${t2 - t1} ms")
-
-    // normal computation
-    val resultList2 = ListBuffer[String]()
-    var t3          = System.currentTimeMillis()
-    1 to 1000 foreach { _ =>
-      resultList2.append(mapFunction(longText + ";"))
-    }
-    var t4 = System.currentTimeMillis()
-    println(s"normal map computation: ${t4 - t3} ms")
-
-    assert(resultList == resultList2)
+    testLocalAsyncMapStream[String, String]((1 to 1000).map(_ => longText + ";").toList,
+                                            singleProcess,
+                                            mapFunction,
+                                            true)
   }
 }
